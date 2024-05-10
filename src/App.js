ab2c637803c657d9e73651d559b1cc4dd14825c2
Avinashhola <avinash.hola9@outlook.com>
2024-05-10 11:40:36 +0530

    all changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import AboutUtensi from './components/AboutUtensi';
+import { useState } from 'react';
+import Alert from './components/Alert';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
+
+  const [mode, setMode] = useState("light")
+  const [alerty, setAlerty] = useState(null)
+
+
+  const showAlert = (message, type) => {
+    setAlerty(
+      {
+        msg: message,
+        type: type
+      }
+    )
+    setTimeout(() => {
+      setAlerty(null);
+    }, 1500);
+  }
+
+
+
+
+
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light")
+      document.body.style.backgroundColor = 'white'
+      showAlert(" Light mode is Enabled", "success")
+      document.title = "UTENSILS---LIGHTMODE"
+    }
+    else {
+      setMode("dark")
+      document.body.style.backgroundColor = 'grey'
+      showAlert(" Dark mode is Enabled", "success")
+      document.title = "UTENSILS---DARKMODE"
+    }
+
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar About="about utensils" mode={mode} toggl={toggleMode} />
+        <Alert alert={alerty} />
+        <Routes>
+          <Route exact path="/Home" element={<Textform mode={mode} showAlertsy={showAlert} />} />
+          <Route exact path="/About" element={<AboutUtensi mode={mode} />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
